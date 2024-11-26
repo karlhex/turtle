7338ddf1fdc0bacfe84b78af85673702aac06142
karlhex <karl_hes@hotmail.com>
2024-11-26 16:34:54 +0800

    add user management

--- src/main/java/com/fwai/turtle/controller/UserController.java
+++ src/main/java/com/fwai/turtle/controller/UserController.java
@@ -3,16 +3,19 @@ package com.fwai.turtle.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.fwai.turtle.persistence.entity.User;
 import com.fwai.turtle.service.interfaces.UserService;
 import com.fwai.turtle.common.ApiResponse;
 import com.fwai.turtle.exception.ResourceNotFoundException;
+
+import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,11 +27,29 @@ public class UserController {
   @Autowired
   private UserService userService;
 
+  @GetMapping
+  public ApiResponse<List<User>> getUsers() {
+    log.info("getUsers");
+    return ApiResponse.ok(userService.findAll());
+  }
+
   @GetMapping("/{id}")
   public ApiResponse<User> getUser(@PathVariable("id") Long id) {
     log.info("getUser: {}", id);
     return ApiResponse.ok(userService.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id)));
+  }
+
+  @GetMapping("/unmapped")
+  public ApiResponse<List<User>> getUnmappedUsers() {
+    log.info("getUnmappedUsers");
+    return ApiResponse.ok(userService.findUnmappedUsers());
+  }
+
+  @PostMapping
+  public ApiResponse<User> createUser(@RequestBody User user) {
+    log.info("createUser: {}", user);
+    return ApiResponse.ok(userService.newUser(user));
   }
 
   @DeleteMapping("/{id}")
