4cab6b7bfd4e87255cce1034d79e5bb786c6102c
KarlHex <karl.hex@gmail.com>
2025-07-21 20:41:53 -0700

    commit before using claude

--- backend/src/main/java/com/fwai/turtle/TurtleApplication.java
+++ backend/src/main/java/com/fwai/turtle/TurtleApplication.java
@@ -2,8 +2,20 @@ package com.fwai.turtle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+    "com.fwai.turtle.base.repository",
+    "com.fwai.turtle.security.repository",
+    "com.fwai.turtle.modules.**.repository"
+})
+@EntityScan(basePackages = {
+    "com.fwai.turtle.base.entity",
+    "com.fwai.turtle.security.entity",
+    "com.fwai.turtle.modules.**.entity"
+})
 public class TurtleApplication {
 
 	public static void main(String[] args) {
